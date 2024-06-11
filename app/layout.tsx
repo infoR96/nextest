91e82e8e853f98fc7f15773fcf8632937c40ee38
Gian <62362598+infoR96@users.noreply.github.com>
2024-06-11 12:22:34 -0500

    test version 1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+
+import '@/app/ui/global.css';
+import  {inter} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* <h1>hello world</h1> */}
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
